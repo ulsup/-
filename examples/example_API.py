a60d97e06d25d3b42712a2b48808b18ebd740db9
ulsup <supruniuk@ucu.edu.ua>
2017-06-07 13:28:57 +0300

    initial commit

---
+++ examples/example_API.py
@@ -0,0 +1,17 @@
+import tmdbsimple as data
+data.API_KEY = '6fe3abae4ea97076bce8490e936ac183'
+
+def example_1():
+    search_1 = data.Search()
+    response_1 = search_1.movie(query='Star Wars')
+    for s in search_1.results:
+        print(s['title'], s['id'], s['release_date'], s['popularity'])
+
+example_1()
+
+def example_2():
+    search_2 = data.Search()
+    response_2 = search_2.person(query='Emma Roberts')
+    print(response_2)
+
+example_2()
